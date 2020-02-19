0e200a3698497df743e65aa68a317301daba5caa
Evan W <evwike@gmail.com>
2020-02-18 21:25:15 -0600

    Refactor, UI improvements

--- app/src/main/java/com/example/tradeblock/MainActivity.java
+++ app/src/main/java/com/example/tradeblock/MainActivity.java
@@ -5,18 +5,32 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_REGISTER = 0;
+
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         checkCurrentUser();
+
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivityForResult(intent, REQUEST_REGISTER);
+            }
+        });
     }
 
     public void checkCurrentUser() {
@@ -24,20 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
         if (user != null) {
             // TODO: User signed in, go to base page
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
         } else {
             Log.i("action", "User not logged in.");
 //            Intent intent = new Intent(this, LoginActivity.class);
 //            startActivity(intent);
-        }
-    }
-
-    public void getUserProfile() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        if (user != null) {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
         }
     }
 }
